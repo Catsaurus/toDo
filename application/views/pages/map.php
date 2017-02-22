bf4111eaf38583fcc9daa8a96d58c2175f5a1a76
Riana <rianarandoja@gmail.com>
2017-02-22 08:36:53 +0200

    see #6

---
+++ application/views/pages/map.php
@@ -0,0 +1,5 @@
+<h1>Where does my Elephant live?</h1>
+
+<div id="elephantData"></div>
+<div id="googleMap"></div>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBnbcCUfwLZEDJOeqGm9VRfJSKeqETl40I&callback=myMap"></script>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -5,6 +5,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <head>
     <link rel="stylesheet" href="<?php echo base_url("asset/css/footer.css"); ?>" />
     <link rel="stylesheet" href="<?php echo base_url("asset/css/start_button.css"); ?>" />
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url("asset/css/Stylesheet.css"); ?>">
+    <script src="<?php echo base_url("asset/javascript/Map.js"); ?>" type="text/javascript"></script>
     <meta charset="utf-8">
     <title>TERE</title>
 
