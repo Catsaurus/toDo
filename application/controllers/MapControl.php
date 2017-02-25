29827bcb2959a9d7ad7d561502e26d6a96e7ca5d
Riana <rianarandoja@gmail.com>
2017-02-25 19:34:59 +0200

    #6 almost done

---
+++ application/controllers/MapControl.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Riana
+ * Date: 22.02.17
+ * Time: 8:39
+ */
+
+class MapControl extends CI_Controller {
+
+    public function index()
+    {
+
+        $this->load->helper('url');
+        $this->load->library('javascript');
+
+        $page = 'map';
+        $data['title'] = ucfirst($page);
+
+        $this->load->view('pages/'.$page, $data);
+        $this->load->view('templates/footer', $data);
+    }
+}
