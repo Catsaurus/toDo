ce5cd72641051ad3bfe3dbaf4a86dcdb09ad40ca
Riana <rianarandoja@gmail.com>
2017-03-09 14:59:54 +0200

    see #32

--- application/views/pages/tasks.php
+++ application/views/pages/tasks.php
@@ -6,29 +6,28 @@
         <h1>Tasks</h1>
 
         <a class="waves-effect waves-teal btn-flat" onclick="showNewTask('newTask')">Add new task</a>
-
         <div class="row" id="newTask">
-            <form class="col s12">
+            <form id="newTaskForm" class="col s12">
                 <div class="row">
                     <div class="input-field col s12">
-                        <input id="description" type="email" class="validate">
+                        <input name="description" id="description" type="text">
                         <label for="description">Description</label>
                     </div>
                 </div>
 
-                <p><input name="groupRepeat" type="radio" id="daily"/>
+                <p><input name="groupRepeat" type="radio" id="daily" value="daily"/>
                     <label for="daily">Daily</label></p>
-                <p><input name  ="groupRepeat" type="radio" id="weekly"/>
+                <p><input name="groupRepeat" type="radio" id="weekly" value="weekly"/>
                     <label for="weekly">Weekly</label></p>
-                <p><input name="groupRepeat" type="radio" id="norepeat" checked="checked"/>
+                <p><input name="groupRepeat" type="radio" id="norepeat" value="norepeat" checked="checked"/>
                     <label for="norepeat">No Repeat</label></p>
 
                 <label> Due date
-                    <input width="50%" type="date" id="date" class="datepicker">
+                    <input name="date" width="50%" type="date" value="<?php echo date('Y-m-d');?>" id="datepicker" class="datepicker">
                 </label>
 
                 <a class="waves-effect waves-teal btn-flat" onclick="hideNewTask('newTask')">Cancel</a>
-                <a class="waves-effect waves-teal btn-flat" onclick="">Add<i class="material-icons right">send</i></a>
+                <a class="waves-effect waves-teal btn-flat" onclick="addTask()">Add<i class="material-icons right">send</i></a>
             </form>
         </div>
 
@@ -37,6 +36,7 @@
         <div class="row" id="taskid">
             <div class="col s4">
                 <p>Due date on täna</p>
+                <p id="tasksOfToday"></p>
             </div>
             <div class="col s4">
                 <p>Due date on sellel nädalal</p>
--- asset/javascript/NewTask.js
+++ asset/javascript/NewTask.js
@@ -9,3 +9,15 @@ function showNewTask(newTask) {
 function hideNewTask(newTask) {
     document.getElementById(newTask).style.display = "none";
 }
+function addTask() {
+
+    var formObj = {};
+    var inputs = $('#newTaskForm').serializeArray();
+
+    $.each(inputs, function (i, input) {
+        formObj[input.name] = input.value;
+        console.log(input.name + " " + input.value);
+    });
+
+    //document.getElementById('tasksOfToday').innerHTML = formObj['description'] + " " + formObj['groupRepeat'] + " " + formObj['date'];
+}
