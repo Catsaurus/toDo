444143530fe8f79fbb3c0f798d7dfbbca9d58db3
Catsaurus <andra0401@hotmail.com>
2017-03-18 11:35:18 +0200

    sign up form

--- application/views/pages/login.php
+++ application/views/pages/login.php
@@ -10,12 +10,10 @@
     </div>
         <div class="container">
             <div class="row">
-
                 <div class="col s3"></div>
-
                     <div class="col s12 m6">
                     <!--Z-DEPTH on z telje sügavus, ehk mida suurem on see number, seda kaugemal on see nö taustast-->
-                        <div class="z-depth-5 row">
+                        <div class="z-depth-3 row">
 
                             <form method="post" action="<?php echo site_url('Login/index'); ?>">
 
@@ -34,22 +32,17 @@
                                             <input class="validate" type="text" name="username" id="username"/>
                                             <label for="username">Your username</label>
                                         </div>
-                                    </div>
-                                    <div class="col s2"></div>
-                                </div>
-
-                                <!--PAROOLI väli-->
-                                <div class="row">
-                                    <div class="col s2"></div>
-                                    <div class="col s8">
                                         <div class="input-field">
                                             <input class="validate" type="password" name="pswd" id="password"/>
                                             <label for="password">Enter password</label>
                                         </div>
+                                            <button type="submit" name="submit" class="col s12 btn btn-large waves-effect">Login</button>
+
+                                        </div>
                                     </div>
                                     <div class="col s2"></div>
-
-
+                                </div>
+                        </form>
                                     <!--FORGOT PASSWORD link-->
                                     <div class="row">
                                         <div class="col s2"></div>
@@ -62,21 +55,9 @@
                                 </div>
 
 
-
-                                <!-- LOGIN nupp-->
-                                <div class="row">
-                                    <div class="col s2"></div>
-                                    <div class="col s8">
-                                        <button type="submit" name="submit" class="col s12 btn btn-large waves-effect">Login</button>
-                                    </div>
-                                    <div class=" col s2"></div>
-                                </div>
-                                <div class="col s3"></div>
-                            </form>
                         </div>
                     </div>
-            </div>
+
             <div class="section"></div>
-        </div>
-    </div>
+
 </main>
--- application/views/pages/signup.php
+++ application/views/pages/signup.php
@@ -6,23 +6,55 @@
                 <h4><?php echo lang('intro')?></h4>
             </div>
         </div>
-        <?php echo validation_errors(); ?>
-        <div class = "signupForm">
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col s3"></div>
+            <div class="col s12 m6">
+                <div class="z-depth-3 row">
+                    <form method="post" action="<?php echo site_url('Register/index') ?>">
 
-            <form method="post" action="<?php echo site_url('Register/index') ?>">
+                        <div class="row">
+                            <div class="col s12"></div>
+                        </div>
 
-                <input type="text" title="Username" name="username"  placeholder="Username" /><br />
+                        <div class="row">
+                            <div class="col s2"></div>
+                            <div class="col s8" >
+                                <?php echo validation_errors(); ?>
 
-                <input type="password" title="Password" name="pswd" placeholder="Password" /><br />
+                                <!--KASUTAJANIMI-->
+                                <div class="input-field">
+                                    <input class="validate" type="text" name="username" id="username">
+                                    <label for="username">Username</label>
+                                </div>
 
-                <input type="password" title="Password again" name="pswd2" placeholder="Password again" /><br />
+                                <!--PAROOL-->
+                                <div class="input-field">
+                                    <input class="validate" type="password" name="pswd" id="password"/>
+                                    <label for="password">Password</label>
+                                </div>
 
-                <input type="email" title="Email" name="email" placeholder="Email" /><br />
+                                <!--PAROOL UUESTI-->
+                                <div class="input-field">
+                                <input class="validate" type="password" name="pswd2" id="password2"/>
+                                <label for="password2">Password again</label>
+                                </div>
 
-                <button type="submit" title="submit" name="submit" value="Register" class="col s12 btn btn-large waves-effect">Sign up</button>
+                                <!--EMAIL-->
+                                <div class="input-field">
+                                    <input class="validate" type="email" title="Email" name="email" id="email">
+                                    <label for="email">Email</label>
+                                </div>
 
+                                <button type="submit" name="submit" class="col s12 btn btn-large waves-effect">Sign up</button>
+                            </div>
+                        </div>
+                        <div class="col s2"></div>
+                    </form>
+                </div>
 
-            </form>
+        </div>
     </div>
     </div>
 </main>
