7aeb9bd57b95c4bcd99f18d8eb4dcc329f020118
Riana <rianarandoja@gmail.com>
2017-03-26 22:16:31 +0300

    removed lightbulb from settings page

--- application/views/pages/settings.php
+++ application/views/pages/settings.php
@@ -4,10 +4,10 @@
 
         <h3>Settings</h3>
         <p></p>
-        <div class="tooltip" id="src2div">
-            <img src="<?php echo base_url("asset/img/lightbulb_on.png"); ?>" id="s2src">
-            <span class="tooltiptext" id="s2"><?php echo lang('change_data') ?></span>
-        </div>
+<!--        <div class="tooltip" id="src2div">-->
+<!--            <img src="--><?php //echo base_url("asset/img/lightbulb_on.png"); ?><!--" id="s2src">-->
+<!--            <span class="tooltiptext" id="s2">--><?php //echo lang('change_data') ?><!--</span>-->
+<!--        </div>-->
         <form id = "changePass" method="post" action="<?php echo site_url('Settings/changePassword') ?>">
         <div class="input-field">
             <input type="password" name="pswd" id="password">
