4e15593221c18c228059083da72ccccecec40768
Mirember <seeteinekolmas@gmail.com>
2017-03-26 13:05:20 +0300

    translations

--- application/language/estonian/general_lang.php
+++ application/language/estonian/general_lang.php
@@ -42,6 +42,10 @@ $lang['due_week'] = 'Tähtaeg on sel nädalal';
 $lang['due_later'] = 'Tähtaeg on hiljem';
 $lang['done'] = 'Ülesanne täidetud';
 $lang['user_tasks'] = 'Sisestatud kohustuste arv: ';
+$lang['long_polling'] = 'Ülesandeid süsteemis kokku';
+$lang['done_tasks'] = 'Tehtud ülesanded';
+$lang['undone_tasks'] = 'Tegemata ülesanded';
+$lang['undone'] = 'Ülesanne tegemata';
 
 // Errors
 $lang['account_exists'] = 'See kasutajanimi on juba võetud. Logi sisse või vali uus kasutajanimi';
@@ -56,34 +60,7 @@ $lang['menu_faq'] = 'KKK';
 $lang['menu_add_news'] = 'Lisa uudis';
 $lang['menu_contact'] = 'Kontakt';
 
-// Main page (table of news)
-$lang['main_title'] = 'Uudised';
-$lang['main_news_id'] = 'ID';
-$lang['main_news_heading'] = 'Pealkiri';
-$lang['main_news_content'] = 'Sisu';
-$lang['main_news_date'] = 'Kuupäev';
-$lang['main_button_load_news'] = 'Laadi vanemaid uudiseid';
-
-// Add news
-$lang['addNews_title'] = 'Lisa uudis';
-$lang['addNews_heading'] = 'Palun sisesta uudis: ';
-$lang['addNews_field_heading_label'] = 'Pealkiri: ';
-$lang['addNews_field_heading_help'] = 'Siia lisa uudise pealkiri';
-$lang['addNews_field_heading_placeholder'] = 'Sisesta siia uudise pealkiri ...';
-$lang['addNews_field_content_label'] = 'Sisu: ';
-$lang['addNews_field_content_help'] = 'Siia lisa uudise sisu';
-$lang['addNews_field_content_placeholder'] = 'Sisesta siia uudise sisu ...';
-$lang['addNews_field_reference_label'] = 'Lisan viite [uudised.err.ee]? ';
-$lang['addNews_field_reference_help'] = 'Kui lisate siia linnukese, siis sisu lõppu lisatakse viide portaalile uudised.err.ee';
-$lang['addNews_submit_button'] = 'Salvesta';
-// Add news validation
-$lang['addNews_val_heading'] = 'pealkiri';
-$lang['addNews_val_content'] = 'sisu';
 
 // FAQ
 $lang['faq_title'] = 'KKK';
 $lang['faq_heading'] = 'Korduma kippuvad küsimused';
-
-// Contact
-$lang['contact_title'] = 'Kontakt';
-$lang['contact_heading'] = 'Kui on küsimusi, siis kirjutage';
