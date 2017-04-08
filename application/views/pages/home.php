137fad9a1c6591c0428af20b7ddd37451a4d1cce
Mirembe/Hiie <seeteinekolmas@gmail.com>
2017-04-08 20:34:44 +0300

    id #37

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -9,6 +9,7 @@
                     <a href="<?php echo site_url('Login/index') ?>" class="waves-effect waves-light btn"><?php echo lang('login') ?></a>
                     <a href="<?php echo site_url('Register/index') ?>" class="waves-effect waves-light btn"><?php echo lang('signup') ?></a>
                     <a onclick="fblogin()" class="waves-effect waves-light btn" id="facebook_button">Facebook</a>
+                    <a href="/idlogin" class="waves-effect waves-light btn">ID</a>
                 </div>
 
                 <div class="col s12">
