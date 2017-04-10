7d69f4819a9d4438819eef804f8a1c976eddc888
Mirembe/Hiie <seeteinekolmas@gmail.com>
2017-04-10 14:25:39 +0300

    ilustamised

--- application/views/pages/sitemap.php
+++ application/views/pages/sitemap.php
@@ -1,4 +1,3 @@
-
 <main>
     <div class="container">
 
@@ -7,9 +6,7 @@
         </div>
 
         <div class="row center_objects">
-            <div class="row">
                 <div class="col s3">
-
                     <ul id="sitemaplinks">
                         <li><a href="<?php echo site_url('Tasks/index') ?>"><?php echo lang('tasks') ?></a></li>
                         <li><a href="<?php echo site_url('Pets/index') ?>"><?php echo lang('pets') ?></a></li>
@@ -19,7 +16,6 @@
                         <li><a href="<?php echo site_url('Logout/index') ?>"><?php echo lang('logout') ?></a></li>
                     </ul>
                 </div>
-            </div>
         </div>
     </div>
 </main>
